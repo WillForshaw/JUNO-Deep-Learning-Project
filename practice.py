7b281ba813ed51072d935a8107fcac504dfd662c
WillForshaw <willforshaw4@gmail.com>
2025-10-18 18:48:34 +0100

    update

---
+++ practice.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Scalar
+scalar = torch.tensor(7)
+print(scalar)
+print(scalar.ndim)
+# Tensors are created using torch.tensor
